53c2bf286dc491a956abd364a2891df6b5e83ab0
svwaltz <97194783+svwaltz@users.noreply.github.com>
2025-08-22 15:02:00 -0700

    adding new database schema

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,12 +2,21 @@ using Microsoft.EntityFrameworkCore;
 using WaltzAndWhisk.Models;
 
 namespace WaltzAndWhisk.Data;
+
 public class AppDbContext : DbContext
 {
-    public DbSet<Recipe> Recipes { get; set; }
 
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
-    {
-    }
+
+    public DbSet<Recipe> Recipes { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<IngredientsSection> IngredientsSections { get; set; }
+    public DbSet<IngredientSubsection> IngredientSubsections { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<InstructionsSection> InstructionsSections { get; set; }
+    public DbSet<InstructionSubsection> InstructionSubsections { get; set; }
+    public DbSet<Instruction> Instructions { get; set; }
+    public DbSet<RecipeImage> RecipeImages { get; set; }
+    public DbSet<Macros> Macros { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 }
--- Migrations/20250822215900_InitialRecipeSchema.Designer.cs
+++ Migrations/20250822215900_InitialRecipeSchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WaltzAndWhisk.Data;
 
@@ -11,9 +12,11 @@ using WaltzAndWhisk.Data;
 namespace WaltzAndWhisk.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250822215900_InitialRecipeSchema")]
+    partial class InitialRecipeSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
